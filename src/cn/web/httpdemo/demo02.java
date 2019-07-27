c01d6d56c2cdddb3ca2fa00a4fdc3a83c7ee4bf6
zm1105 <827556302@qq.com>
2019-07-27 10:09:58 +0800

    Initial commit

---
+++ src/cn/web/httpdemo/demo02.java
@@ -0,0 +1,23 @@
+package cn.web.httpdemo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo02")
+public class demo02 extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        System.out.println("dopost");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("doget01");
+    }
+}
